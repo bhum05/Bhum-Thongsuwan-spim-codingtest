aefbeb61020099624a864c4f7e74838c2d38e333
Bhum08 <72738381+bhum05@users.noreply.github.com>
2021-08-20 17:59:40 +0700

    Add files via upload

---
+++ index2.js
@@ -0,0 +1,5 @@
+let firstword = prompt("คําแรก");
+let secondword = prompt("คําที่สอง");
+
+alert(firstword + secondword);
+console.log(firstword + secondword);
---
+++ indexข้อ1.js
@@ -0,0 +1,6 @@
+function test(A,B,C) {
+    answer = A**3+B**2+C
+    console.log(answer)
+}
+let answer1 = test(2,3,4);
+let answer2 = test(5,9,3);
